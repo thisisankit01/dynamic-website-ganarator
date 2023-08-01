0a91793f1d8690336318c7be4e02c6506a804daa
Ankit Pandey <93221382+thisisankit01@users.noreply.github.com>
2023-08-01 09:15:12 +0530

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -2,20 +2,18 @@ import axios from "axios";
 
 export async function fetchData(maxRetries: number = 3): Promise<any> {
   const axiosInstance = axios.create({
-    timeout: 5000, // Set a longer timeout value (in milliseconds)
+    timeout: 5000,
   });
 
   let retries = 0;
   while (retries < maxRetries) {
     try {
-      // Simulate a 5-second delay in the response
+       respon
       await new Promise((resolve) => setTimeout(resolve, 5000));
 
-      // Replace the following line with the actual API call
       const res = await axiosInstance.get(
         `https://www.boredapi.com/api/activity`
       );
-      // For testing purposes, return mock data instead of making the actual API call
       const mockData = {
         activity: "Mock Activity",
         type: "mock",
